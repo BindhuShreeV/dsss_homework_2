4519c706fc02f72dee0cf8ae003b5bbb56445994
ma41rode <113033725+BindhuShreeV@users.noreply.github.com>
2024-11-04 16:33:58 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz_package",
+    version="1.0",
+    packages=find_packages(),               # Automatically find packages in the directory
+    install_requires=open("requirements.txt").read().splitlines(),  # Include requirements
+    entry_points={
+        "console_scripts": [
+            "math_quiz=math_quiz_package.math_quiz:math_quiz",  # Adjusted for new package path
+        ],
+    },
+    author="Your Name",
+    description="A simple math quiz application",
+)
